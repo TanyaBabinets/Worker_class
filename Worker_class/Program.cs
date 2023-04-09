7ce3f420f419d4cec50dd4679d2693b8ad077427
Tanyababinets <tanya060616@gmail.com>
2023-04-09 14:18:51 +0300

    Добавьте файлы проекта.

---
+++ Worker_class/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Задание 6.4
+//Создать абстрактный базовый класс Worker (работника) с методом Print(). 
+// Создайте четыре производных класса: President, Security, Manager, Engineer.
+//Переопределите метод Print() для вывода информации, соответствующей каждому типу работника.
+namespace Worker_class
+{
+    abstract class Worker 
+        {
+        abstract public void Print();
+        }
+    class President : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("President of company Nikolay Ivanovich Dimov ");
+        }
+    }
+    class Security:Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Security works 24 hours 7 days/week ");
+        }
+
+    }
+    class Manager:Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Manager controls all prosecces ");
+        }
+
+    }
+    class Engeneer : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Engeneer is responsible for technical support ");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            President p = new President();
+            p.Print();
+            Security s = new Security();
+            s.Print();
+            Manager m = new Manager();
+            m.Print();
+            Engeneer en = new Engeneer();
+            en.Print();
+
+        }
+    }
+}
---
+++ Worker_class/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Worker_class")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Worker_class")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4b4d5d7b-f8fe-497c-a994-632826c945b6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
